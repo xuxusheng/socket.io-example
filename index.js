a7d7c0a1f6ec9453bce6a27c334b0469126a98d7
许盛 <xusheng@antiy.cn>
2017-08-19 18:08:52 +0800

    over

--- index.js
+++ index.js
@@ -1,25 +1,75 @@
-const app = require('express')()
-const http = require('http').Server(app)
-const io = require("socket.io")(http)
+// Setup basic express server
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+var port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  // res.send('<h1>Hello world</h1>')
-  res.sendFile(__dirname + '/index.html')
+server.listen(port, () => {
+  console.log('Server listening at port %d', port)
 })
+
+// Routing
+app.use(express.static(__dirname + '/public'))
+
+// Chatroom
+
+let numUsers = 0;
 
 io.on('connection', socket => {
-  console.log('a user connected')
+  let addedUser = false
   
-  socket.on('chat message', msg => {
-    console.log('message: ', msg)
-    io.emit('chat message', msg)
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', data => {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    })
   })
   
+  // 当收到客户端 'add user' 消息时
+  socket.on('add user', username => {
+    if (addedUser) return
+    
+    // we store the username in the socket session for this client
+    socket.username = username
+    ++numUsers
+    addedUser = true
+    socket.emit('login', {
+      numUsers: numUsers
+    })
+    
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    })
+  })
+  
+  // when the client emits 'typing', we broadcast ti to others
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    })
+  })
+  
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    })
+  })
+  
+  // when the user disconnects.. perform this
   socket.on('disconnect', () => {
-    console.log('a user disconnected')
+    if (addedUser) {
+      --numUsers
+    }
+    // echo globally that this client has left
+    socket.broadcast.emit('user left', {
+      username: socket.username,
+      numUsers: numUsers
+    })
   })
-})
-
-http.listen(3000, () => {
-  console.log('listening on *.3000...')
 })
